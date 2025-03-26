82045371fdb16582eb6aa3cba26a97f9b633df34
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-03-26 22:43:06 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/CCDA_to_OMOP_multi_transform.py
+++ transforms-python/src/myproject/datasets/CCDA_to_OMOP_multi_transform.py
@@ -1,10 +1,31 @@
-# from pyspark.sql import functions as F
-from transforms.api import transform_df, Input, Output
+
+from transforms.api import transform, Input, Output
+from pyspark.sql import types as T
+from pyspark.sql import functions as F
 
 
-dataset_name  = 'ccda_response_files'
+import os
 
-@transform_df(
+
+def process_dataset_of_files_initial(dataset):
+    ## A TransformInput object does not have an attribute files. Please check the spelling and/or the datatype of the object.
+    ccda_documents_generator = dataset.files()  ## <---- no attribute files!
+    for filegen in ccda_documents_generator:
+        filepath = filegen.download()
+        print(f"PROCESSING {os.path.basename(filepath)}")
+
+## def process_dataset_of_files(dataset):
+
+
+    
+def create_empty_df(ctx):
+    schema = T.StructType([T.StructField("key", T.StringType(), True)])
+    df = ctx.spark_session.createDataFrame([("dummy_key",)], schema)
+    df = df.withColumn('when', F.current_timestamp())
+    return df
+ 
+
+@transform(
     condition_occurrence = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/condition_occurrence"),
     drug_exposure = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/drug_exposure"),
     location = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/location"),
@@ -15,18 +36,38 @@ dataset_name  = 'ccda_response_files'
     provider = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/provider"),
     visit_occurrence = Output("ri.foundry.main.dataset.1fc47371-d39c-4985-aacd-c0aacf5484b3"),
 
-    xml_files = Input("/All of Us-cdb223/HIN - HIE/sharedResources/FullyIdentified/ccda_response_files"),
-    metadata = Input("/All of Us-cdb223/HIN - HIE/sharedResources/FullyIdentified/ccda_response_metadata"),
+    xml_files=Input("ri.foundry.main.dataset.8c8ff8f9-d429-4396-baed-a3de9c945f49"),
+    # xml_files = Input("/All of Us-cdb223/HIN - HIE/sharedResources/FullyIdentified/ccda/ccda_response_files"),
+    metadata = Input("ri.foundry.main.dataset.672dd7ae-bbd4-43e8-9b8b-b5c7e8711e79"),
+    # metadata = Input("/All of Us-cdb223/HIN - HIE/sharedResources/FullyIdentified/ccda/ccda_response_metadata"),
     visit_xwalk = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/visit_concept_xwalk_mapping_dataset"),
     codemap_xwalk = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk"),
-    valueset_xwalk = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/ccda_value_set_mapping_table_dataset"),
+    valueset_xwalk = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/ccda_value_set_mapping_table_dataset")
 )
-
 def compute(
-    condition_occurrence,  drug_exposure, location, 
-    measurement, observation, person,
-    procedure_occurrence, provider, visit_occurrence,
-    xml_files, metadata, visit_xwalk,
-    codemap_xwalk, valueset_xwalk ):
+    ctx,
+    # outputs
+        condition_occurrence,  drug_exposure, location, 
+        measurement, observation, person,
+        procedure_occurrence, provider, visit_occurrence,
+    # inputs
+        xml_files, 
+        metadata, visit_xwalk,
+        codemap_xwalk, valueset_xwalk ):
+
+    #process_dataset_of_files(xml_files)
+
+    fdf = create_empty_df(ctx)
+    condition_occurrence.write_dataframe(fdf)
+    drug_exposure.write_dataframe(fdf)
+    location.write_dataframe(fdf)
+    measurement.write_dataframe(fdf)
+    observation.write_dataframe(fdf)
+    person.write_dataframe(fdf)
+    procedure_occurrence.write_dataframe(fdf)
+    provider.write_dataframe(fdf)
+    visit_occurrence.write_dataframe(fdf)
+
+
 
     
