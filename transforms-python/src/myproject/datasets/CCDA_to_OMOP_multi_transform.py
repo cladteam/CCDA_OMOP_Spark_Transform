46c8520348a8a21f2d8b960f9fee2d9c143dffe1
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-03-28 21:31:39 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/CCDA_to_OMOP_multi_transform.py
+++ transforms-python/src/myproject/datasets/CCDA_to_OMOP_multi_transform.py
@@ -37,22 +37,8 @@ def convert_and_write(ctx, name, dataset_dict, spark_ds):
     provider = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/provider"),
     visit_occurrence = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/visit_occurrence"),
 
-
-    #care_site = Output("ri.foundry.main.dataset.4c563173-2281-4e0c-99e1-f11ea21f8eb6"),
-    #condition_occurrence = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/condition_occurrence"),
-    #drug_exposure = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/drug_exposure"),
-    #location = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/location"),
-    #measurement = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/measurement"),
-    #observation = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/observation"),
-    #person = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/person"),
-    #procedure_occurrence = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/procedure_occurrence"),
-    #provider = Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP/provider"),
-    #visit_occurrence = Output("ri.foundry.main.dataset.1fc47371-d39c-4985-aacd-c0aacf5484b3"),
-
     xml_files=Input("ri.foundry.main.dataset.8c8ff8f9-d429-4396-baed-a3de9c945f49"),
-    # xml_files = Input("/All of Us-cdb223/HIN - HIE/sharedResources/FullyIdentified/ccda/ccda_response_files"),
     metadata = Input("ri.foundry.main.dataset.672dd7ae-bbd4-43e8-9b8b-b5c7e8711e79"),
-    # metadata = Input("/All of Us-cdb223/HIN - HIE/sharedResources/FullyIdentified/ccda/ccda_response_metadata"),
     visit_xwalk_ds = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/visit_concept_xwalk_mapping_dataset"),
     codemap_xwalk_ds = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk"),
     valueset_xwalk_ds = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/ccda_value_set_mapping_table_dataset")
@@ -68,6 +54,7 @@ def compute(
         xml_files, 
         metadata, visit_xwalk_ds,
         codemap_xwalk_ds, valueset_xwalk_ds ):
+        
     global codemap_xwalk
     global ccda_value_set_mapping_table_dataset
     global visit_concept_xwalk_mapping_dataset
@@ -130,7 +117,7 @@ def compute(
 #    convert_and_write(ctx, 'Person',      omop_dataset_dict, person)
 #    convert_and_write(ctx, 'Procedure',   omop_dataset_dict, procedure_occurrence)
 #    convert_and_write(ctx, 'Provider',    omop_dataset_dict, provider)
-    convert_and_write(ctx, 'Visit',       omop_dataset_dict, visit_occurrence)
+#    convert_and_write(ctx, 'Visit',       omop_dataset_dict, visit_occurrence)
 #    convert_and_write(ctx, 'Care_Site',   omop_dataset_dict, care_site)
 
 
