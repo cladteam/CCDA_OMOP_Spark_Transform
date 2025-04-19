3da043d166542f6c29fee3d5dfe565bd015a08fd
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-19 17:48:31 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
@@ -15,9 +15,8 @@ def compute(source_df, codemap):
 
     df = df.join(codemap, (df.condition_concept_source_system == codemap.src_vocab_code_system) & \
                           (df.condition_concept_source_code == codemap.src_code) ) 
-    df = df.select('condition_source_value', 'condition_concept_source_system', 'condition_concept_source_code', \
-                        'target_concept_id', 'target_domain_id', 'source_concept_id')
 
+    df = df.withColumn('condition_concept_id', df.source_concept_id)
 
     return df
 
