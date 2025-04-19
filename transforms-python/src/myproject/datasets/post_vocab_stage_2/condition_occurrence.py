356479c14966c669001d58cc2759c17b17f66811
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-19 16:26:09 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
@@ -5,13 +5,16 @@ from transforms.api import transform_df, Input, Output
 
 @transform_df(
     Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/post_vocab_stage_2/condition_occurrence"),
-    source_df=Input("ri.foundry.main.dataset.e34c8928-d1c1-4b4e-8026-e6024e6afdbb")
-    #,codemap_xwalk_ds = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
+    source_df=Input("ri.foundry.main.dataset.e34c8928-d1c1-4b4e-8026-e6024e6afdbb"),
+    codemap = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
 )
-def compute(source_df):
+def compute(source_df, codemap):
     split_source_value = F.split(source_df.condition_source_value, '\\|') # splits on a regex, escape the 'or'
     df = source_df.withColumn('condition_concept_source_system', split_source_value.getItem(0)) \
                 .withColumn('condition_concept_source_code', split_source_value.getItem(1)) \
-                .select('condition_source_value', 'condition_concept_source_system', 'condition_concept_source_code')
+                .join(codemap, source_df.condition_concept_source_sytem == codemap.src_vocab_code_system and \
+                               source_df.condition_concept_source_code == codemap.src_code) \
+                .select('condition_source_value', 'condition_concept_source_system', 'condition_concept_source_code', \
+                        'target_concept_id', 'target_domain_id', 'source_concept_id')
     return df
 
