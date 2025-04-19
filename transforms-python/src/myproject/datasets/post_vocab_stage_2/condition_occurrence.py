644273bc18b63dce6ac24e4b4f4017595a270834
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-19 17:03:41 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
@@ -11,8 +11,7 @@ from transforms.api import transform_df, Input, Output
 def compute(source_df, codemap):
     split_source_value = F.split(source_df.condition_source_value, '\\|') # splits on a regex, escape the 'or'
     df = source_df.withColumn('condition_concept_source_system', split_source_value.getItem(0)) \
-                  .withColumn('condition_concept_source_code', split_source_value.getItem(1)) \
-                  .limit(100)
+                  .withColumn('condition_concept_source_code', split_source_value.getItem(1))
 
     #df = df.join(codemap, (df.condition_concept_source_system == codemap.src_vocab_code_system) & \
     #                      (df.condition_concept_source_code == codemap.src_code) )
