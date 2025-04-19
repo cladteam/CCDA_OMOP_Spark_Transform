17326a56296924a64f32355181fbcf0f756fc7b1
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-19 15:42:23 +0000

    Build Commit

---
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/condition_occurrence.py
@@ -0,0 +1,14 @@
+from pyspark.sql import functions as F
+from transforms.api import transform_df, Input, Output
+
+
+@transform_df(
+    Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/post_vocab_stage_2/condition_occurrence"),
+    source_df=Input("ri.foundry.main.dataset.e34c8928-d1c1-4b4e-8026-e6024e6afdbb")
+    #,codemap_xwalk_ds = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
+)
+def compute(source_df):
+    split_source_value = F.split(source_df['condition_source_value'], '|')
+    source_df.withColumn('condition_concept_source_system', split_source_value.getItem(0)) \
+             .withColumn('condition_concept_source_code', split_source_value.getItem(1))
+    return source_df
