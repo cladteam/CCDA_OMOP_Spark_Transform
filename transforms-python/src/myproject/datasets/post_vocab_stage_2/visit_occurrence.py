fe652deee12c87dc8691ab6771afcc122a8a9dcb
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-21 17:31:26 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/visit_occurrence.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/visit_occurrence.py
@@ -4,20 +4,11 @@ from transforms.api import transform_df, Input, Output
 
 @transform_df(
     Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/post_vocab_stage_2/visit_occurrence"),
-    visits = Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/visit_occurrence"),
-    codemap = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
+    visits = Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/visit_occurrence")
 )
-def compute(visits, codemap):
-    split_source_value = F.split(visits.visit_source_value, '\\|') # splits on a regex, escape the 'or'
-    df = visits.withColumn('visit_concept_source_system', split_source_value.getItem(1)) \
-                  .withColumn('visit_concept_source_code', split_source_value.getItem(0))
-
-    df = df.join(codemap, (df.visit_concept_source_system == codemap.src_vocab_code_system) & \
-                          (df.visit_concept_source_code == codemap.src_code) ) 
-
-    df = df.withColumn('visit_concept_id', df.source_concept_id)
-
-    df = df.select([
+def compute(visits):
+    
+    df = visits.select([
         'visit_source_value', 'person_id', 'visit_occurrence_id', 'visit_source_concept_id',
         'preceding_visit_occurrence_id', 'discharge_to_concept_id', 'admitting_source_concept_id',
         'care_site_id', 'provider_id', 'visit_concept_id', 'visit_start_date', 'visit_start_datetime',
