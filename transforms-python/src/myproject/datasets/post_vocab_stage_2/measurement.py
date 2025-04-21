5617078163f7da0b50bcc691a0c05e4c9707be88
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-21 19:17:31 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/measurement.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/measurement.py
@@ -14,7 +14,7 @@ def compute(measurements, codemap):
 
     df = df.join(codemap, (df.measurement_concept_source_system == codemap.src_vocab_code_system) & \
                           (df.measurement_concept_source_code == codemap.src_code) ) \
-            .select(df['*'], codemap['target_concept_id', 'target_domain_id', 'source_concept_id'])
+            .select(df['*'], codemap['target_concept_id', 'target_domain_id', 'source_concept_id']) 
 
     df = df.withColumn('measurement_concept_id', df.target_concept_id)
     df = df.withColumn('measurement_source_concept_id', df.source_concept_id)
