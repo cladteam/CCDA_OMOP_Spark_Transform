debf9b93eaac8337cbb4f2e4c7126f25fe1022f9
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-21 15:52:28 +0000

    Build Commit

---
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/care_site.py
@@ -0,0 +1,22 @@
+from pyspark.sql import functions as F
+from transforms.api import transform_df, Input, Output
+
+
+@transform_df(
+    Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/care_site"),
+    source_df=Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/post_vocab_stage_2/care_site"),
+    codemap=Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
+)
+def compute(source_df, codemap):
+    split_source_value = F.split(source_df.place_of_service_source_value, '\\|') # splits on a regex, escape the 'or'
+    df = source_df.withColumn('place_of_service_source_system', split_source_value.getItem(1)) \
+                  .withColumn('place_of_service_source_code', split_source_value.getItem(0))
+
+    df = df.join(codemap, (df.place_of_service_source_system == codemap.src_vocab_code_system) & \
+                          (df.place_of_service_source_code == codemap.src_code) ) 
+
+    df = df.withColumn('place_of_service_concept_id', df.source_concept_id)
+
+    df = df.dropColumns(['place_of_service_source_system', 'place_of_serivce_source_code'])
+
+    return df
