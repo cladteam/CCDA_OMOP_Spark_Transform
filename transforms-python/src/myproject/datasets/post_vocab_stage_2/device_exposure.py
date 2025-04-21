7bafc3f8056f9958cd24b14dd54a6b8c3cc0abb4
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-21 17:23:37 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/device_exposure.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/device_exposure.py
@@ -3,7 +3,7 @@ from transforms.api import transform_df, Input, Output
 
 
 @transform_df(
-    Output("/All of Us-cdb223/HIN - HIE/CCDA/datasets/post_vocab_stage_2/device_exposure"),
+    Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentfiedData/OMOP_spark/post_vocab_stage_2/device_exposure"),
     devices = Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/device_exposure"),
     codemap = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
 )
--- transforms-python/src/myproject/datasets/post_vocab_stage_2/drug_exposure.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/drug_exposure.py
@@ -3,7 +3,7 @@ from transforms.api import transform_df, Input, Output
 
 
 @transform_df(
-    Output("/All of Us-cdb223/HIN - HIE/CCDA/datasets/post_vocab_stage_2/drug_exposure"),
+    Output("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/post_vocab_stage_2/drug_exposure"),
     drugs = Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/drug_exposure"),
     codemap = Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
 )
