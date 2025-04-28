76f799695765738c5cde4f9e6a049e812fb491ba
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-28 19:24:31 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/uniquify_stage_1/drug_exposure.py
+++ transforms-python/src/myproject/datasets/uniquify_stage_1/drug_exposure.py
@@ -4,7 +4,7 @@ from transforms.api import transform_df, Input, Output
 
 @transform_df(
     Output("/All of Us-cdb223/HIN - HIE/CCDA/datasets/uniquify_stage_1/drug_exposure"),
-    source_input = Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/drug_exposure")
+    source_df = Input("/All of Us-cdb223/HIN - HIE/CCDA/IdentifiedData/OMOP_spark/drug_exposure")
 )
 def compute(source_df):
     return source_df.dropDuplicates()
