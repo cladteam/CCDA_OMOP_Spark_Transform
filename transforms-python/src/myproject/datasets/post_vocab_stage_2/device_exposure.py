6f17a28b3d9392e5931e5b6a51fa63e84544b439
Chris Roeder <27caf55b-22a1-4fd3-a21a-8401c0ae33fd>
2025-04-28 21:32:17 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/post_vocab_stage_2/device_exposure.py
+++ transforms-python/src/myproject/datasets/post_vocab_stage_2/device_exposure.py
@@ -8,7 +8,7 @@ from transforms.api import transform_df, Input, Output
     codemap=Input("/All of Us-cdb223/HIN - HIE/CCDA/transform/mapping-reference-files/codemap_xwalk")
 )
 def compute(devices, codemap):
-    split_source_value = F.split(devices.condition_source_value, '\\|') # splits on a regex, escape the 'or'
+    split_source_value = F.split(devices.device_source_value, '\\|') # splits on a regex, escape the 'or'
     df = devices.withColumn('device_concept_source_system', split_source_value.getItem(1)) \
                   .withColumn('device_concept_source_code', split_source_value.getItem(0))
 
